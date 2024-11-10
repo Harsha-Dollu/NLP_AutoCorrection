0b46cf0fc6b1f3219fa4acd7fa58280470d9aceb
Dollu Harsha vardhan <163315273+Harsha-Dollu@users.noreply.github.com>
2024-11-10 18:17:42 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -1,16 +1,14 @@
-# main_script.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Add this import
 from autocorrection import Autocorrection
 
-# Initialize Flask app
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-# Initialize the Autocorrection model with a sample vocabulary file
 checker = Autocorrection("words.txt")
 
-# Route for autocorrect API
+
 @app.route('/autocorrect', methods=['POST'])
 def autocorrect():
     try:
@@ -20,7 +18,7 @@ def autocorrect():
         if not input_text:
             return jsonify({"error": "Input text is required."}), 400
 
-        # Split input into words and process each word
+       
         words = input_text.split()
         corrected_words = []
         corrections = []
@@ -32,7 +30,7 @@ def autocorrect():
                 
             correction = checker.correct_spelling(word.lower())
             
-            # If corrections are found, use the highest probability correction
+     
             if correction and len(correction) > 0:
                 best_correction = max(correction, key=lambda x: x[1])
                 corrected_word = best_correction[0]
